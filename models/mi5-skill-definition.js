0c2c64a64589385a5c1d0e6ee132391a762762c4
Jakob Procher <procher@itq.de>
2017-11-23 20:58:02 +0100

    wip

---
+++ models/mi5-skill-definition.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+
+const hasState = require('./mi5-hasState');
+
+class Skill extends hasState {
+    /**
+     * 
+     * @param {any} Module
+     * @param {any} SkillName
+     * @param {any} [InputParameter]
+     * @param {any} [OutputParameter]
+     * @param {any} [InitState]
+     */
+    constructor(Module, SkillName = '', InputParameter = [], OutputParameter = [], InitState = 'Aborted') {
+        this.SkillName = SkillName;
+        this.InputParameter = InputParameter;
+        this.OutputParameter = OutputParameter;
+        this.State = State;
+
+        
+
+    }
+
+        
+
+}
