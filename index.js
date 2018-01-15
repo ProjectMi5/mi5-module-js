a49daec08b0a6b1d298d1ed8972ba3feb6a004d6
Dominik Serve <serve@itq.de>
2018-01-15 16:42:59 +0100

    add mqtt publisher

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.Mi5Module = require('./models/mi5-module');
 module.exports.Mi5Skill = require('./models/mi5-skill');
+module.exports.Mi5mqttPublisher = require('./lib/mqttPublisher');
