6b9f2a8038589e51b2daa675f4f2a8fde174bc8f
Dominik Serve <serve@itq.de>
2018-03-08 21:32:08 +0100

    add missing transitions

--- default-configurations/smart4i/ValidStateTransitions.js
+++ default-configurations/smart4i/ValidStateTransitions.js
@@ -2,9 +2,9 @@ const addClusterStateTransitions = require('../../helpers/resolveClusters');
 
 const clusters = {
   /*whiteCluster: ["Running", "Paused", "Pausing"],
-  transparentCluster: ["whiteCluster", "Suspending", "Suspended", "Unsuspending"],
-  yellowCluster: ["transparentCluster", "Idle", "Starting", "Completing", "Complete", "Resetting", "Holding", "Held", "Unholding"],
-  redCluster: ["yellowCluster", "Clearing", "Stopped", "Stopping"]*/
+  transparentCluster: ["whiteCluster", "Suspending", "Suspended", "Unsuspending"],*/
+  yellowCluster: [13, 0, 1, 2, 11, 12],
+  redCluster: ["yellowCluster", 18, 15]
 };
 
 let StateTransitions = {
@@ -44,10 +44,10 @@ let StateTransitions = {
   12: {
     reset: 13
   },
-  /*
-  Aborting: {
-    done: "Aborted"
-  },*/
+  //Aborting
+  16: {
+    done: 17
+  },
   // Aborted
   17: {
     clear: 18,
@@ -73,6 +73,7 @@ let StateTransitions = {
   18: {
     done: 15 // Stopped
   },
+  //Resetting
   13: {
     done: "0" // Idle
   },/*
@@ -81,13 +82,13 @@ let StateTransitions = {
   },
   transparentCluster: {
     hold: "Holding"
-  },
-  yellowCluster: {
+  },*/
+  /*yellowCluster: {
     stop: "Stopping"
-  },
+  },*/
   redCluster: {
-    abort: "Aborting"
-  }*/
+    abort: 16
+  }
 };
 
 addClusterStateTransitions(StateTransitions, clusters);
